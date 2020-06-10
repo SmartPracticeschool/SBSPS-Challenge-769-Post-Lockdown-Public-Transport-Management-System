92b9cfe3795ad7d295fcf1fc3bb3f5037f6503a4
Naveen <naveensharma10d@gmail.com>
2020-06-10 07:30:20 +0530

    Initial App Settings

--- app/src/main/java/com/example/e_transpo/MainActivity.java
+++ app/src/main/java/com/example/e_transpo/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.e_transpo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView user_img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user_img = (ImageView) findViewById(R.id.imageView);
+        user_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent i = new Intent(MainActivity.this, user_activity.class);
+                        startActivity(i);
+                        finish();
+                    }
+                }, 1000);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/e_transpo/user_activity.java
@@ -0,0 +1,13 @@
+package com.example.e_transpo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class user_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_activity);
+    }
+}
