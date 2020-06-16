8770a8f49fe8c391793832657ffed18810b79457
Naveen <naveensharma10d@gmail.com>
2020-06-16 17:20:32 +0530

    Ticket Generation Done with Barcode

--- app/src/main/java/com/example/e_transpo/ticket_booking.java
+++ app/src/main/java/com/example/e_transpo/ticket_booking.java
@@ -49,12 +49,28 @@ public class ticket_booking extends AppCompatActivity implements View.OnClickLis
         mAuth = FirebaseAuth.getInstance();
         userid = mAuth.getCurrentUser().getUid();
         searchablespinner = (Spinner) findViewById(R.id.searchablespinner);
-        List<String> searchablespinner_list = new ArrayList<String>();
-        searchablespinner_list.add("Select Train/Bus/Metro Number");
-        searchablespinner_list.add("22439-Vande Bharat Express");
-        searchablespinner_list.add("12049-Gatimaan Express");
-        searchablespinner_list.add("12002-New Delhi Bhopal Shatabdi Express ");
-        searchablespinner_list.add("12951-Mumbai New Delhi Rajdhani Express");
+        final List<String> searchablespinner_list_train = new ArrayList<String>();
+        searchablespinner_list_train.add("Select Train/Bus/Metro Number");
+        searchablespinner_list_train.add("22439-Vande Bharat Express");
+        searchablespinner_list_train.add("12049-Gatimaan Express");
+        searchablespinner_list_train.add("12002-New Delhi Bhopal Shatabdi Express ");
+        searchablespinner_list_train.add("12951-Mumbai New Delhi Rajdhani Express");
+
+        final List<String> searchablespinner_list_bus = new ArrayList<String>();
+        searchablespinner_list_bus.add("Bus ID 10001");
+        searchablespinner_list_bus.add("Bus ID 10002");
+        searchablespinner_list_bus.add("Bus ID 10003");
+        searchablespinner_list_bus.add("Bus ID 10004");
+        searchablespinner_list_bus.add("Bus ID 10005");
+
+        final List<String> searchablespinner_list_Metro = new ArrayList<String>();
+        searchablespinner_list_Metro.add("Metro ID 50001");
+        searchablespinner_list_Metro.add("Metro ID 50002");
+        searchablespinner_list_Metro.add("Metro ID 50003");
+        searchablespinner_list_Metro.add("Metro ID 50004");
+        searchablespinner_list_Metro.add("Metro ID 50005");
+
+
         mode_of_transpo = (Spinner) findViewById(R.id.mode_of_transpo);
         List<String> searchablespinnermode_list = new ArrayList<String>();
         searchablespinnermode_list.add("Select Mode Of Transport");
@@ -65,71 +81,90 @@ public class ticket_booking extends AppCompatActivity implements View.OnClickLis
         mode_of_transpo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String smode = parent.getItemAtPosition(position).toString();
-                modeid = smode;
+                if (position == 1) {
+                    String smode = parent.getItemAtPosition(position).toString();
+                    modeid = smode;
+                    searchablespinner.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, searchablespinner_list_bus));
+                }
+                if (position == 2) {
+                    String smode = parent.getItemAtPosition(position).toString();
+                    modeid = smode;
+                    searchablespinner.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, searchablespinner_list_train));
+                }
+                if (position == 3) {
+                    String smode = parent.getItemAtPosition(position).toString();
+                    modeid = smode;
+                    searchablespinner.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, searchablespinner_list_Metro));
+                }
+
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
-        searchablespinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, searchablespinner_list));
         searchablespinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String snumber = parent.getItemAtPosition(position).toString();
-                Transportid = snumber;
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {String smode = parent.getItemAtPosition(position).toString();
+                String modeid = parent.getItemAtPosition(position).toString();
+                Transportid = modeid ;
+
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
-    }
-    private void validation() {
-        String date_string = date.getText().toString();
-        String from_string = from.getText().toString();
-        String to_string = to.getText().toString();
-        String tickets_string = no_of_ticket.getText().toString();
-        if (date_string.isEmpty()) {
-            date.setError("Please input date");
-            date.requestFocus();
-            return;
-        }
-        if (from_string.isEmpty()) {
-            from.setError("Field mendatory");
-            from.requestFocus();
-            return;
-        }
-        if (to_string.isEmpty()) {
-            to.setError("Field mendatory");
-            to.requestFocus();
-            return;
-        }
-        if (tickets_string.isEmpty()) {
-            no_of_ticket.setError("Field mendatory");
-            no_of_ticket.requestFocus();
-            return;
-        }
-
-        Toast.makeText(getApplicationContext(),date_string +" "+ from_string+ " " + to_string+ " " + tickets_string+ " " + Transportid + " " +modeid ,Toast.LENGTH_SHORT).show();
-        Intent i = new Intent(ticket_booking.this,ticket_finalisation.class);
-        i.putExtra("passed_date",date_string);
-        i.putExtra("passed_from",from_string);
-        i.putExtra("passed_to",to_string);
-        i.putExtra("passed_ticket",tickets_string);
-        i.putExtra("passed_transpoid",Transportid);
-        i.putExtra("passed_modeid",modeid);
-        i.putExtra("passed_userid",userid);
-        startActivity(i);
-
 
     }
+        private void validation () {
+            String date_string = date.getText().toString();
+            String from_string = from.getText().toString();
+            String to_string = to.getText().toString();
+            String tickets_string = no_of_ticket.getText().toString();
+            if (date_string.isEmpty()) {
+                date.setError("Please input date");
+                date.requestFocus();
+                return;
+            }
+            if (from_string.isEmpty()) {
+                from.setError("Field mendatory");
+                from.requestFocus();
+                return;
+            }
+            if (to_string.isEmpty()) {
+                to.setError("Field mendatory");
+                to.requestFocus();
+                return;
+            }
+            if (tickets_string.isEmpty()) {
+                no_of_ticket.setError("Field mendatory");
+                no_of_ticket.requestFocus();
+                return;
+            }
+
+            Toast.makeText(getApplicationContext(), date_string + " " + from_string + " " + to_string + " " + tickets_string + " " + Transportid + " " + modeid, Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(ticket_booking.this, ticket_finalisation.class);
+            i.putExtra("passed_date", date_string);
+            i.putExtra("passed_from", from_string);
+            i.putExtra("passed_to", to_string);
+            i.putExtra("passed_ticket", tickets_string);
+            i.putExtra("passed_transpoid", Transportid);
+            i.putExtra("passed_modeid", modeid);
+            i.putExtra("passed_userid", userid);
+            startActivity(i);
+
+
+        }
+
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
+        switch (v.getId()){
             case R.id.booktickrt_btn:
                 validation();
+                break;
         }
+
     }
 }
--- app/src/main/java/com/example/e_transpo/ticket_finalisation.java
+++ app/src/main/java/com/example/e_transpo/ticket_finalisation.java
@@ -1,23 +1,44 @@
 package com.example.e_transpo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
-public class ticket_finalisation extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ticket_finalisation extends AppCompatActivity implements View.OnClickListener {
     Button pay_button;
-    TextView transpo_name;
+    TextView transpo_name,date_textview,userid_textview,cost_textview;
     ImageView barcode_view;
+    private ProgressBar loading;
+    Integer Cost_value;
+    Integer cost_metro=35,cost_bus=100,cost_train=150;
+    FirebaseAuth mauth;
+    FirebaseFirestore fbase;
+    String USER_id,mainticketid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,28 +46,75 @@ public class ticket_finalisation extends AppCompatActivity {
         setContentView(R.layout.activity_ticket_finalisation);
         pay_button = (Button)findViewById(R.id.pay_btn);
         transpo_name = (TextView) findViewById(R.id.transpo_name);
+        date_textview = (TextView) findViewById(R.id.date_finalticket);
+        userid_textview = (TextView) findViewById(R.id.finalticket_userid);
+        cost_textview = (TextView) findViewById(R.id.cost);
         barcode_view = (ImageView)findViewById(R.id.bcode_img) ;
-        barcode_generate();
-
+        loading = (ProgressBar)findViewById(R.id.loader);
+        pay_button.setOnClickListener(this);
+        mauth = FirebaseAuth.getInstance();
+        fbase = FirebaseFirestore.getInstance();
+        ticket_generate();
     }
 
-    private void barcode_generate() {
+    private void ticket_generate() {
+        Calendar calendar = Calendar.getInstance();
         String _transpo_id = getIntent().getStringExtra("passed_transpoid");
         String _mode_id = getIntent().getStringExtra("passed_modeid");
         String _user_id = getIntent().getStringExtra("passed_userid");
-        String _no_ticket = getIntent().getStringExtra("passed_ticket");
+        Integer _no_ticket = Integer.parseInt(getIntent().getStringExtra("passed_ticket"));
         String _to = getIntent().getStringExtra("passed_to");
         String _from = getIntent().getStringExtra("passed_from");
         String _date = getIntent().getStringExtra("passed_date");
-        String text = "TRANSPPORT ID -- "+_transpo_id+" "+"MODE ID -- "+_mode_id+"USER ID -- "+ _user_id+" FROM -- "+_from +" TO -- "+_to+" DATE -- "+_date+" NO OF TICKETS -- "+_no_ticket;
+        transpo_name.setText("Passenger Ticket For" + _transpo_id);
+        date_textview.setText("Date :   "+_date);
+        userid_textview.setText("User Id :  "+_user_id);
+        mainticketid = _mode_id+_user_id+calendar.getTimeInMillis();
+        if(_mode_id.contentEquals("Metro")) {
+            Cost_value = _no_ticket * cost_metro;
+            cost_textview.setText("Total cost = "+Cost_value+"Rs,"+cost_metro+"Rs. per head");
+        }
+        if(_mode_id.contentEquals("Bus")) {
+            Cost_value = _no_ticket  * cost_bus;
+            cost_textview.setText("Total cost = "+Cost_value+"Rs,"+cost_bus+"Rs. per head");
+        }
+        if(_mode_id.contentEquals("Train")) {
+            Cost_value =  _no_ticket  * cost_train;
+            cost_textview.setText("Total cost = "+Cost_value+"Rs,"+cost_train+"Rs. per head");
+        }
+
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
         try {
-            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,400,400);
+            BitMatrix bitMatrix = multiFormatWriter.encode(mainticketid, BarcodeFormat.QR_CODE,350,350);
             BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
             Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
             barcode_view.setImageBitmap(bitmap);
         } catch (WriterException e) {
             e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.pay_btn:
+                loading.setVisibility(View.VISIBLE);
+                FirebaseUser user = mauth.getCurrentUser();
+                USER_id = mauth.getCurrentUser().getUid();
+                DocumentReference documentReference = fbase.collection("Govt_Ticket_detais").document();
+                Map<String,Object> usermap = new HashMap<>();
+                usermap.put(USER_id,mainticketid);
+                documentReference.set(usermap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        loading.setVisibility(View.GONE);
+                        Toast.makeText(getApplicationContext(),"Ticket saved successfully",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+                break;
         }
 
     }
